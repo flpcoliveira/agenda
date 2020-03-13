42ec2f959c482b4070c178472608d98ac150c26b
Felipe Cristo <felipe.cristo@hotmail.com>
2020-03-12 23:50:00 -0300

    Create mapping profile

---
+++ Agenda.Api/Mappings/MappingProfile.cs
@@ -0,0 +1,24 @@
+using Agenda.Api.Infrastructure.Entities;
+using Agenda.Api.Models;
+using AutoMapper;
+
+namespace Agenda.Api.Mappings
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateAppointmentsMap();
+        }
+
+        private void CreateAppointmentsMap()
+        {
+            CreateMap<Appointment, AppointmentDto>()
+            .ForMember(dest => dest.PatientId, opt => opt.MapFrom(src => src.Patient.Id)).ReverseMap()
+            .ForPath(dest => dest.Patient.Id, opt => opt.MapFrom(src => src.PatientId))
+            .ForPath(dest => dest.Patient.Name, opt => opt.MapFrom(src => src.PatientName))
+            .ForPath(dest => dest.Patient.BirthDate, opt => opt.MapFrom(src => src.PatientBirthDate));
+        }
+
+    }
+}
