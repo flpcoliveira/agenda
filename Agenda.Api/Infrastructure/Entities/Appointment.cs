35335f11342d864988461ec8f1aea073859254da
Felipe Cristo <felipe.cristo@hotmail.com>
2020-03-14 18:56:20 -0300

    Change fields to insert data correctly

--- Agenda.Api/Infrastructure/Entities/Appointment.cs
+++ Agenda.Api/Infrastructure/Entities/Appointment.cs
@@ -8,9 +8,11 @@ namespace Agenda.Api.Infrastructure.Entities
 {
     public class Appointment
     {
-        public int Id { get; set; }
+        public int? Id { get; set; }
 
         [Required]
+        public int PatientId { get; set; }
+
         public Patient Patient { get; set; }
 
         [Required]
