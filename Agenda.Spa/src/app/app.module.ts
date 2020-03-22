c371604d974c15bb65d4778e72d916611722fa66
flpcoliveira <felipe.cristo@hotmail.com>
2020-03-22 11:31:41 -0300

    Define angular material design

--- Agenda.Spa/src/app/app.module.ts
+++ Agenda.Spa/src/app/app.module.ts
@@ -6,16 +6,32 @@ import { AppComponent } from './app.component';
 import { AppointmentService } from './services/appointment.service';
 import { HttpClientModule } from '@angular/common/http';
 import { AppointmentListComponent } from './appointments/list/appointment-list.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NavigationComponent } from './navigation/navigation.component';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AppointmentListComponent
+    AppointmentListComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
-    HttpClientModule
+    HttpClientModule,
+    BrowserAnimationsModule,
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule
   ],
   providers: [AppointmentService],
   bootstrap: [AppComponent]
